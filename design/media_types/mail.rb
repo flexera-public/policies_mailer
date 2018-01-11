c134c3832596ad514c496fe0f57e2d420dc2ec46
Richard Shade <rshade@rightscale.com>
2018-01-11 17:09:30 -0500

    updating docs

--- design/media_types/mail.rb
+++ design/media_types/mail.rb
@@ -5,13 +5,13 @@
         identifier 'application/json'
 
         attributes do
-          attribute :message_id, String
-          attribute :message, String
+          attribute :message_id, String, example: 'Queued. Thank you.'
+          attribute :message, String, example: "<20180111191946.1.50D7896597E97DC2@example.com>"
         end
 
         view :default do
-          attribute :message, example: 'Queued. Thank you.'
-          attribute :message_id, example: "<20180111191946.1.50D7896597E97DC2@example.com>"
+          attribute :message
+          attribute :message_id
         end
       end
     end
